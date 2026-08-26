c1078667c2ebfd980e144390efe6f53d75a30493
omarstudent36-alt <omarstudent36@gmail.com>
2026-08-26 15:15:58 +0200

    first commit

---
+++ number-nine/main.js
@@ -0,0 +1,43 @@
+var firstqautision=prompt("what is the cabital of france")
+
+if(firstqautision=="Baris"){
+    console.log("true answer") 
+    
+} else{
+        console.log("wrong")
+   
+    }
+     var age=prompt("your age")
+      if(age>="18"){
+        console.log("allowed")
+    }else{
+        console.log("not allowed")
+    }
+
+    var bestplayer=prompt("best player ever")
+    if(bestplayer == "Ronaldo"){
+        console.log("true")
+    }else{
+        console.log("false")
+    }
+
+    var password=confirm("password contains 6 numbers")
+    if(password == true){
+        console.log("true value")
+    }else{
+        console.log("false value")
+    }
+
+    var username=prompt("your username")
+    var confirmpass=confirm("is pass 123456")
+
+    if(username=="Omar"){
+           console.log("email is true")
+    } else if(confirmpass == true){
+        console.log("pass is true")
+       }
+    else{
+       console.log("pass or username false")
+    }
+    
+
